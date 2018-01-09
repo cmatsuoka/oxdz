118ae2bb151c465230b3231e698f958c920aa593
Claudio Matsuoka <cmatsuoka@gmail.com>
2018-01-08 22:18:12 -0200

    Add interpolator tests and fix 8-bit linear interpolator
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/mixer/interpolator.rs
+++ src/mixer/interpolator.rs
@@ -48,7 +48,7 @@ impl InterpolatorBase for Linear {
 impl Interpolate<i8> for Linear {
     fn get_sample(&self, i: &[i8], frac: i32) -> i32 {
         let l1 = (i[1] as i32) << 8;
-        let dt = (i[2] as i32) << 8 - l1;
+        let dt = ((i[2] as i32) << 8) - l1;
         l1 as i32 + (((frac >> 1) * dt as i32) >> (SHIFT - 1)) as i32
     }
 }
@@ -61,3 +61,44 @@ impl Interpolate<i16> for Linear {
     }
 }
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_interpolate_nearest_i8() {
+        let interp = NearestNeighbor;
+        let i: &[i8] = &[0, 0x10, 0x40, 0x70];
+        assert_eq!(interp.get_sample(i, 0), 0x1000);
+        assert_eq!(interp.get_sample(i, 32767), 0x1000);
+        assert_eq!(interp.get_sample(i, 65535), 0x1000);
+    }
+
+    #[test]
+    fn test_interpolate_nearest_i16() {
+        let interp = NearestNeighbor;
+        let i: &[i16] = &[0, 0x1000, 0x4000, 0x7000];
+        assert_eq!(interp.get_sample(i, 0), 0x1000);
+        assert_eq!(interp.get_sample(i, 32767), 0x1000);
+        assert_eq!(interp.get_sample(i, 65535), 0x1000);
+    }
+
+    #[test]
+    fn test_interpolate_linear_i8() {
+        let interp = Linear;
+        let i: &[i8] = &[0, 0x10, 0x40, 0x70];
+        assert_eq!(interp.get_sample(i, 0), 0x1000);
+        assert_eq!(interp.get_sample(i, 32767), 0x27ff);
+        assert_eq!(interp.get_sample(i, 65535), 0x3fff);
+    }
+
+    #[test]
+    fn test_interpolate_linear_i16() {
+        let interp = Linear;
+        let i: &[i16] = &[0, 0x1000, 0x4000, 0x7000];
+        assert_eq!(interp.get_sample(i, 0), 0x1000);
+        assert_eq!(interp.get_sample(i, 32767), 0x27ff);
+        assert_eq!(interp.get_sample(i, 65535), 0x3fff);
+    }
+}
